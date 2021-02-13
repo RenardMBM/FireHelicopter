14c2db0406eabef4a4d4b7b85132d5a505286c46
Fedya Zuev <zuev.f.s@gmail.com>
2021-02-13 16:13:08 +1000

    Add Levers script and modificate Helicopter

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -18,7 +18,7 @@ public class Helicopter : MonoBehaviour
     }
     void Update()
     {
-        transform.Translate(new Vector3(_leftLever.Rotation.eulerAngles.x, 0, _leftLever.Rotation.eulerAngles.z % 90) * Time.deltaTime);
+        transform.Translate(new Vector3(_leftLever.Rotation.eulerAngles.x, 0, _leftLever.Rotation.eulerAngles.z) * Time.deltaTime);
         Debug.Log(new Vector3(_leftLever.Rotation.eulerAngles.x, 0, _leftLever.Rotation.eulerAngles.z));
         if (_numFail == 3)
         {
