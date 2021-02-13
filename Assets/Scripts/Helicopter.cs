74b90bbeb265a041591c06ba0bfb7806deb24157
RenardMBM <renard.mbm@yandex.ru>
2021-02-13 15:29:39 +1000

    Methods for counting extinguishing and missed files - Helicopter.cs /.idea/ to ignore - .gitignore

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Helicopter : MonoBehaviour
 {
+    private int _numSuccess;
+    private int _numFail;
 
     void Start()
     {
-
+        _numSuccess = 0;
+        _numFail = 0;
     }
-
-
     void Update()
     {
-        
+        if (_numFail == 3)
+        {
+            // TODO: GameOver
+        }
     }
+
+    public int GETNumSuccess() { return _numSuccess; }
+    public void Fail() { _numFail++; }
+    public void Success() { _numSuccess++; }
 }
