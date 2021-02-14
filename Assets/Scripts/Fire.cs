c0557a063fff76b5485206ef0af1e3509602bcd2
RenardMBM <renard.mbm@yandex.ru>
2021-02-14 13:13:42 +1000

    Fire timer - Fire.cs FireLabel - Label.cs

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -6,14 +6,27 @@ using UnityEngine;
 public class Fire : MonoBehaviour
 {
     public float time;
-    IEnumerator TimerLife(float time)
+    IEnumerator TimerLife()
     {
-        yield return new WaitForSeconds(time);
-        gameObject.GetComponentInParent<BadWolf>().Fail();
-        Destroy(gameObject);
+        yield return new WaitForSeconds(0.01f);
+        if (time <= 0f)
+        {
+            gameObject.GetComponentInParent<BadWolf>().Fail();
+            Destroy(gameObject);
+        }
+        else
+        {
+            time -= 0.01f;
+            GetComponentInChildren<Label>().Time = time;
+            StartCoroutine(TimerLife());
+        }
     }
-    
-    private void Start() { StartCoroutine(TimerLife(time)); }
+
+    private void Start()
+    {
+        GetComponentInChildren<Label>().Time = time;
+        StartCoroutine(TimerLife());
+    }
     
     public void Kill()
     {
---
+++ Assets/Scripts/Label.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Label : MonoBehaviour
+{
+    public GameObject Player;
+    private float _time;
+    public float Time { set => _time = value; }
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        GetComponentInChildren<Text>().text = _time.ToString("0.00");
+    }
+    
+}
